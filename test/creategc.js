e62bc7475014c07bf8947e046465e68515f1e39e
sidorares <sidorares@yandex.ru>
2011-07-14 18:08:25 +1000

    CreateGC request

---
+++ test/creategc.js
@@ -0,0 +1,16 @@
+var x11 = require('../lib/x11');
+
+var xclient = x11.createClient();
+var mapped = true;
+xclient.on('connect', function(display) {
+    var X = this;
+    var root = display.screen[0].root;
+    var wid = X.AllocID();
+
+    X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: 0, eventMask: 0x00000040 });
+    X.MapWindow(wid);
+  
+    var cid = X.AllocID();
+    X.CreateGC(cid, wid);  
+    
+});
