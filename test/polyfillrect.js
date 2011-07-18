fa75b915993fb44776a1fff76027cc3ad34bb912
sidorares <sidorares@yandex.ru>
2011-07-18 17:16:25 +1000

    PolyFillRectangle test

---
+++ test/polyfillrect.js
@@ -0,0 +1,34 @@
+var x11 = require('../lib/x11');
+
+var xclient = x11.createClient();
+var Exposure = x11.eventMask.Exposure;
+var PointerMotion = x11.eventMask.PointerMotion;
+
+xclient.on('connect', function(display) {
+    var X = this;
+    var root = display.screen[0].root;
+    var white = display.screen[0].white_pixel;
+    var black = display.screen[0].black_pixel;
+
+    var wid = X.AllocID();
+    X.CreateWindow(
+       wid, root, 
+       10, 10, 400, 300, 
+       1, 1, 0,
+       { 
+           backgroundPixel: white, eventMask: Exposure|PointerMotion  
+       }
+    );
+    X.MapWindow(wid);
+  
+    var gc = X.AllocID();
+    X.CreateGC(gc, wid, { foreground: black, background: white } );
+    X.on('event', function(ev) {
+        if (ev.type == 12)
+        {
+            X.PolyFillRectangle(wid, gc, [20, 30, 50, 90]); 
+        } else if (ev.type == 6) {
+            console.log(ev.x, ev.y);
+        }
+    });
+});
