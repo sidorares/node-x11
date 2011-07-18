ebb8dfaabfc063e8dad59a16ee5612d07b9ddba1
sidorares <sidorares@yandex.ru>
2011-07-18 17:16:54 +1000

    event mask symbolic names; PolyFillRectangle request

--- test/creategc.js
+++ test/creategc.js
@@ -1,16 +1,17 @@
 var x11 = require('../lib/x11');
 
 var xclient = x11.createClient();
+var PointerMotion = x11.eventMask.PointerMotion;
 var mapped = true;
+
 xclient.on('connect', function(display) {
     var X = this;
     var root = display.screen[0].root;
-    var wid = X.AllocID();
+    var white = display.screen[0].white_pixel;
+    var black = display.screen[0].black_pixel;
 
-    X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: 0, eventMask: 0x00000040 });
-    X.MapWindow(wid);
-  
+    var wid = X.AllocID();
+    X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: white, eventMask: PointerMotion }); 
     var cid = X.AllocID();
-    X.CreateGC(cid, wid);  
-    
+    X.CreateGC(cid, wid);     
 });
--- test/createwindow.js
+++ test/createwindow.js
@@ -1,17 +1,19 @@
 var x11 = require('../lib/x11');
 
 var xclient = x11.createClient();
+var PointerMotion = x11.eventMask.PointerMotion;
 
 xclient.on('connect', function(display) {
     var X = this;
     var root = display.screen[0].root;
     var wid = X.AllocID();
+    var white = display.screen[0].white_pixel;
+    var black = display.screen[0].black_pixel;
 
-    X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: 0, eventMask: 0x00000040 });
+    X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: white, eventMask: PointerMotion });
     X.MapWindow(wid);
 
     var wid1 = X.AllocID();    
-    X.CreateWindow(wid1, root, 10, 10, 40, 30, 1, 1, 0, { backgroundPixel: xclient.display.screen[0].white_pixel, eventMask: 0x00000040 });
+    X.CreateWindow(wid1, root, 10, 10, 40, 30, 1, 1, 0, { backgroundPixel: black, eventMask: PointerMotion });
     X.MapWindow(wid1);
-
 });
