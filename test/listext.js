359c8205efa203e2e7ca8b78afad7b56d9dc16d7
Andrey Sidorov <sidorares@yandex.ru>
2011-07-19 09:31:47 +1000

    XClient.terminate

--- test/listext.js
+++ test/listext.js
@@ -2,7 +2,9 @@ var x11 = require('../lib/x11');
 var X = x11.createClient();
 X.on('connect', function(display) {
     X.ListExtensions(function(list) {
-        console.log(list);
-        X.close();
+        list.forEach(function(ext) {
+            console.log(ext);
+        });
+        X.terminate();
     });
 });
