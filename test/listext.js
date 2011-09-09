437f9b1270586d6acb3a0cd42be2975240b02084
CrypticSwarm <crypticswarm+git@gmail.com>
2011-09-09 07:16:20 -0500

    brings test/testext to new api

--- test/listext.js
+++ test/listext.js
@@ -1,10 +1,10 @@
 var x11 = require('../lib/x11');
 var X = x11.createClient();
 X.on('connect', function(display) {
-    X.ListExtensions(function(list) {
-        list.forEach(function(ext) {
-            console.log(ext);
-        });
-        X.terminate();
+  X.ListExtensions(function(list) {
+    list.names.forEach(function(ext) {
+      console.log(ext.name);
     });
+    X.terminate();
+  });
 });
