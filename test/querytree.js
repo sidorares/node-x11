ec330996ce451145e9e16ae8f0896876880e199f
Andrey Sidorov <sidorares@yandex.ru>
2012-02-28 17:19:44 +1100

    querytree example

---
+++ test/querytree.js
@@ -0,0 +1,11 @@
+var x11 = require('../lib/x11');
+var wid = process.argv[2];
+console.log(wid);
+var wids = [];
+x11.createClient(function(display) {
+    var X = display.client;
+    var root = display.screen[0].root;
+    X.QueryTree(wid ? wid : root, function(tree) {
+        console.log(tree);    
+    });
+});
