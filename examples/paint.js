91a3e5e08f898dbcaf5419f331c24cb00592ad62
Andrey Sidorov <sidorares@yandex.ru>
2012-04-11 13:05:23 +1000

    createwindow depth parameter updated

--- examples/paint.js
+++ examples/paint.js
@@ -16,7 +16,7 @@ var xclient = x11.createClient(function(display) {
         
         var white = display.screen[0].white_pixel;
         var black = display.screen[0].black_pixel;
-        X.CreateWindow(wid, root, 10, 10, 400, 300, 1, 1, 0, { backgroundPixel: white, eventMask: PointerMotion|ButtonPress|ButtonRelease });
+        X.CreateWindow(wid, root, 10, 10, 400, 300, 0, 0, 0, 0, { backgroundPixel: white, eventMask: PointerMotion|ButtonPress|ButtonRelease });
         X.MapWindow(wid);
 
         var pict = X.AllocID();
@@ -63,7 +63,3 @@ var xclient = x11.createClient(function(display) {
         });
     });
 });
-
-function updateScene()
-{
-}
