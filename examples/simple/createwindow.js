95765c938573664664c15661f3f91198ed2a22b1
Andrey Sidorov <sidorares@yandex.ru>
2012-05-01 13:10:09 +1000

    clean up (use default parameters)

--- examples/simple/createwindow.js
+++ examples/simple/createwindow.js
@@ -1,7 +1,14 @@
 var x11 = require('../../lib/x11');
 x11.createClient(function(display) {
+    console.log(display.screen[0])
     var X = display.client;
     var wid = X.AllocID();
-    X.CreateWindow(wid, display.screen[0].root, 100, 100, 400, 300);
+    X.CreateWindow(wid, display.screen[0].root, 100, 100, 400, 300,
+    11,
+    32,
+    0,
+    613
+    );
     X.MapWindow(wid);
+    X.on('error', function(err) { console.log(err) });
 });
