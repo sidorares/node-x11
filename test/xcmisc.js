f40eb4e3d27b86b0c11f8fb148afe92ec0f434ed
Andrey Sidorov <sidorares@yandex.ru>
2012-05-31 11:06:46 +1000

    XC-MISC extension

---
+++ test/xcmisc.js
@@ -0,0 +1,19 @@
+var x11 = require('../lib/x11');
+
+x11.createClient(function(display) {
+    var X = display.client;
+    var root = display.screen[0].root;
+    X.require('xc-misc', function(Misc) {
+        var xid = X.AllocID();
+        console.log("first ID from connection: " + xid);
+        debugger;
+        Misc.GetXIDRange(function(err, range) {
+           console.log("ID range from GetIDRange: [start:" + range.startId + ", count: " + range.count + "]");
+        });
+        Misc.GetXIDList(100, function(err, list) {
+           console.log("ID list from GetIDList(100) : " + list);
+        });
+    });
+    X.on('error', function(err) { console.log("Error", err); });
+ 
+});
