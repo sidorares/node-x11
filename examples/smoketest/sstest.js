58e526d913a8a2a876a32f794894c38b77c3afe1
Andrey Sidorov <sidorares@yandex.ru>
2012-06-08 14:57:24 +1000

    SCREEN-SAVER extension

---
+++ examples/smoketest/sstest.js
@@ -0,0 +1,28 @@
+var x11 = require('../../lib/x11');
+
+x11.createClient(function(display) {
+    var X = display.client;
+    var root = display.screen[0].root;
+    //X.ForceScreenSaver(0);
+    X.SetScreenSaver(20, 10, 2, 2);
+
+
+    X.require('screen-saver', function(SS) {
+        SS.SelectInput(root, SS.eventMask.Notify|SS.eventMask.Cycle);
+        //console.log(SS);
+        //setTimeout(function() {
+        //    X.ForceScreenSaver(1);
+        //}, 5000);
+        setInterval(function() {
+        SS.QueryInfo(root, function(err, info) {
+            console.log(info.until);    
+            //SS.SelectInput(root, SS.eventMask.Notify|SS.eventMask.Cycle);
+        });
+        }, 1000);
+        X.on('event', function(ev) {
+            console.log(ev);
+        });
+    });
+    X.on('error', function(err) { console.log(err); });
+ 
+});
