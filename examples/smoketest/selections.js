06d87c59085346a1facd7358af58fb52f1a7c2ae
Andrey Sidorov <sidorares@yandex.ru>
2012-06-09 14:15:19 +1000

    Get/SetSelectionOwner requests

---
+++ examples/smoketest/selections.js
@@ -0,0 +1,13 @@
+var x11 = require('../../lib/x11');
+
+x11.createClient(function(display) {
+    var X = display.client;
+    var id = parseInt(process.argv[2]);
+    var root = display.screen[0].root;
+    X.SetSelectionOwner(root, X.atoms.PRIMARY);
+    X.GetSelectionOwner(X.atoms.PRIMARY, function(err, win) {
+        console.log(err, win, root);
+    });
+    X.on('event', console.log);
+    X.on('error', console.error);
+});
