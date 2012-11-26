c473c9cad7128f800c29554686e956460adbec12
Andrey Sidorov <sidorares@yandex.com>
2012-11-27 10:42:35 +1100

    initial implementation for fixes extension

---
+++ examples/smoketest/fixestest.js
@@ -0,0 +1,17 @@
+var x11 = require('../../lib/x11');
+
+x11.createClient(function(display) {
+    var X = display.client;
+    var root = display.screen[0].root;
+    X.require('fixes', function(Fixes) {
+        console.log(Fixes);
+        var win = X.AllocID();
+        X.CreateWindow(win, root, 0, 0, 100, 100);
+        //Fixes.ChangeSaveSet(win, Fixes.SaveSetMode.Insert, Fixes.SaveSetTarget.Nearest, Fixes.SaveSetMap.Map);
+        X.on('event', function(ev) {
+          console.log(ev);
+        });
+    });
+    X.on('error', function(err) { console.log(err); });
+
+});
