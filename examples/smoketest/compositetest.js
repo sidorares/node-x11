acfa90a2641abbed6b66459727bc8e054e863ae6
Andrey Sidorov <sidorares@yandex.com>
2012-11-27 11:30:48 +1100

    fix path

--- examples/smoketest/compositetest.js
+++ examples/smoketest/compositetest.js
@@ -1,18 +1,38 @@
-var x11 = require('../lib/x11');
+var x11 = require('../../lib/x11');
 
 x11.createClient(function(display) {
     var X = display.client;
     var root = display.screen[0].root;
     X.require('composite', function(Composite) {
-        console.log(Composite);
-        //Composite.GetOverlayWindow(root, function(overleyid) {
-        //  console.log(overlayid);
+      X.require('damage', function(Damage) {
+        var wid = parseInt(process.argv[2]);
+        //Composite.GetOverlayWindow(wid, function(err, overlayid) {
+        //  console.log("OVERLAY:", err, overlayid);
         //});
-        Composite.RedirectWindow(root, Composite.Redirect.Automatic);
+        //Composite.RedirectWindow(wid, Composite.Redirect.Automatic);
+        var pixmap = X.AllocID();
+        Composite.NameWindowPixmap(wid, pixmap);
+        var damage = X.AllocID();
+        Damage.Create(damage, wid, Damage.ReportLevel.NonEmpty);
+
+
+        var newwin = X.AllocID();
+        X.CreateWindow(newwin, display.screen[0].root, 200, 200, 200, 200, 0, 0, 0, 0, { eventMask: x11.eventMask.Exposure})
+        var gc = X.AllocID();
+        X.CreateGC(gc, newwin);
+        X.MapWindow(newwin);
+
         X.on('event', function(ev) {
           console.log(ev);
+          if (ev.type == 13)
+             return;
+          Damage.Subtract(damage, 0, 0);
+          // srcDrawable, dstDrawable, gc, srcX, srcY, dstX, dstY, width, height
+          X.CopyArea(wid, newwin, gc, 0, 0, 0, 0, 200, 200);
+          //X.CopyArea(pixmap, newwin, gc, 0, 0, 0, 0, 200, 200);
         });
+      });
     });
     X.on('error', function(err) { console.log(err); });
- 
+
 });
