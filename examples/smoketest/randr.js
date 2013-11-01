c6dcb2b67daa303b1b49c94157b83af45f884638
Andrey Sidorov <sidorares@yandex.ru>
2013-11-01 13:05:14 +1100

    initiall support for RANDR extension

---
+++ examples/smoketest/randr.js
@@ -0,0 +1,15 @@
+var x11 = require('../../lib');
+
+x11.createClient(function(err, display) {
+    var X = display.client;
+    var root = display.screen[0].root;
+    X.require('randr', function(Randr) {
+        //console.log(Randr);
+        //Randr.QueryVersion(1, 4, console.log);
+        Randr.SelectInput(root, Randr.NotifyMask.ScreenChange);
+        X.on('event', function(ev) {
+          console.log(ev);
+        });
+    });
+    X.on('error', function(err) { console.log(err); });
+});
