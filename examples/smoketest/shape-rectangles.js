749b877d9901ede5748104deabcd4571e3a7301b
Michael J M Thomson <megadonkey@users.noreply.github.com>
2015-05-29 16:15:03 +1000

    Added Shape rectangles example. Fixed shape mask test

--- examples/smoketest/shape-rectangles.js
+++ examples/smoketest/shape-rectangles.js
@@ -7,13 +7,10 @@ x11.createClient(function(err, display) {
     var root = display.screen[0].root;
     X.require('shape', function(err, Shape) {
         var win = X.AllocID();
-        var black = display.screen[0].black_pixel;
-        X.CreateWindow(win, root, 0, 0, 200, 200, 0, undefined, X.InputOutput, undefined,
-                       { backgroundPixel: black });
+        X.CreateWindow(win, root, 0, 0, 200, 200);
+        X.ChangeWindowAttributes(win, { backgroundPixel: display.screen[0].black_pixel });
         X.MapWindow(win);
-
-        Shape.Rectangles(Shape.Op.Set, Shape.Kind.Bounding, win,
-                         0, 0, [
+        Shape.Rectangles(Shape.Op.Set, Shape.Kind.Bounding, win, 0, 0, [
             [40, 40, 40, 40], [120, 40, 40, 40],
             [0, 120, 20, 20], [180, 120, 20, 20],
             [20, 140, 30, 20], [150, 140, 30, 20],
