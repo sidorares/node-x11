fafa42e6e67b62e84c385521eeab152f7b5bf932
Michael J M Thomson <megadonkey@users.noreply.github.com>
2015-05-29 14:28:05 +1000

    Add Shape Rectangles test. Fix Shape Mask test

--- examples/smoketest/shapetest.js
+++ examples/smoketest/shapetest.js
@@ -6,17 +6,17 @@ x11.createClient(function(err, display) {
     X.require('shape', function(err, Shape) {
         var win = X.AllocID();
         X.CreateWindow(win, root, 0, 0, 200, 200);
-        var gc = X.AllocID();
-        X.CreateGC(gc, win);
-        //X.MapWindow(win);
+        X.MapWindow(win);
         Shape.SelectInput(win, 1);
         Shape.InputSelected(win, function(err, isSelected) {
             console.log("IsSelected: " + isSelected);
         });
-        //var pid = X.AllocID();
-        //X.CreatePixmap(pid, win, 2, 200, 200);
-        //X.PolyText8(pid, gc, 0, 0, ['Hello, Node.JS!', ' Hello, world!']);
-        //Shape.Mask(Shape.Op.Set, Shape.Kind.Input, win, 0, 0, pid);
+        var pid = X.AllocID();
+        X.CreatePixmap(pid, win, 1, 200, 200);
+        var gc = X.AllocID();
+        X.CreateGC(gc, win);
+        X.PolyText8(pid, gc, 0, 0, ['Hello, Node.JS!', ' Hello, world!']);
+        Shape.Mask(Shape.Op.Set, Shape.Kind.Input, win, 0, 0, pid);
 
         X.on('event', function(ev) {
           console.log(ev);
