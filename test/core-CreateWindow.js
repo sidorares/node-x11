dbe2d97beebd25c24e8b1970ea6b4bbcd8ec96e8
Santiago Gimeno <santiago.gimeno@ionide.es>
2015-10-01 11:46:13 +0200

    src: fix some more events parsing
    - DestroyNotify, UnmapNotify, MapNotify and MapRequest.
    - Add some tests.

--- test/core-CreateWindow.js
+++ test/core-CreateWindow.js
@@ -63,18 +63,41 @@ describe('CreateWindow request', function() {
       var root = display.screen[0].root;
       X.ChangeWindowAttributes(root, { eventMask: x11.eventMask.SubstructureNotify });
       X.on('event', function(ev) {
-        ev.name.should.equal('CreateNotify');
-        ev.parent.should.equal(root);
-        ev.wid.should.equal(wid);
-        ev.x.should.equal(0);
-        ev.y.should.equal(0);
-        ev.width.should.equal(1);
-        ev.height.should.equal(1);
-        ev.borderWidth.should.equal(0);
-        ev.overrideRedirect.should.equal(false);
-        done();
+        switch (ev.name) {
+          case 'CreateNotify':
+            ev.parent.should.equal(root);
+            ev.wid.should.equal(wid);
+            ev.x.should.equal(0);
+            ev.y.should.equal(0);
+            ev.width.should.equal(1);
+            ev.height.should.equal(1);
+            ev.borderWidth.should.equal(0);
+            ev.overrideRedirect.should.equal(false);
+          break;
+
+          case 'MapNotify':
+            ev.event.should.equal(root);
+            ev.wid.should.equal(wid);
+            ev.overrideRedirect.should.equal(false);
+            X.UnmapWindow(wid);
+          break;
+
+          case 'UnmapNotify':
+            ev.event.should.equal(root);
+            ev.wid.should.equal(wid);
+            ev.fromConfigure.should.equal(false);
+            X.DestroyWindow(wid);
+          break;
+
+          case 'DestroyNotify':
+            ev.event.should.equal(root);
+            ev.wid.should.equal(wid);
+            done();
+          break;
+        }
       });
 
       X.CreateWindow(wid, root, 0, 0, 1, 1); // 1x1 pixel window
+      X.MapWindow(wid);
   })
 });
